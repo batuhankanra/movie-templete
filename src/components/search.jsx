bd55b53202831fa9fe1a72998b918f13de5bd197
batuhan <107402252+batuhankanra@users.noreply.github.com>
2024-10-23 15:06:45 +0300

    search uptaded

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -3,6 +3,8 @@ import { FaSearch } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { filterMovies } from "../store/movie";
 
+const apiImg=import.meta.env.VITE_API_IMAGE_URL
+
 export default function Search() {
   const [search,setSearch]=useState('')
   const {data,filterData}=useSelector(state=>state.movie)
@@ -23,10 +25,16 @@ export default function Search() {
           <input type="text" value={search} onChange={handleSearch} placeholder='Search' className="flex-1 text-black border py-3 px-2 font-medium rounded-md focus:border-indigo-500 border-zinc-300 outline-none " />
           <button className=" p-4 bg-indigo-500 text-white rounded-full disabled:bg-red-600" disabled={!search} ><FaSearch className="text-lg" /></button>
         </div>
-        {filterData.length<20 || filterData.length>0 && (
-          <div className="absolute -bottom-2 bg-white text-black w-full p-1 right-0 rounded-md">
-          ssa
-        </div>
+        {filterData.length<20 && filterData.length>0 && (
+          <div className="absolute z-10 w-full bg-white border-gray-300 rounded-md  text-black">
+            {filterData.map(data=>(
+              <div className="p-3 flex items-center gap-x-2 border-y hover:bg-gray-200 duration-150 transition-all ">
+                <img src={`${apiImg}/${data.poster_path}`}  className="h-20" />
+
+                <h1>{data.title}</h1>
+              </div>
+            ))}
+          </div>
         )}
         
       </div>
