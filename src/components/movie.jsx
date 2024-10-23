bc686dd8b979e2af5cd41b599c8fb767f4d645af
batuhan <107402252+batuhankanra@users.noreply.github.com>
2024-10-23 23:54:45 +0300

    add pagination

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { getMovieApi } from "../store/movie";
@@ -7,8 +7,9 @@ const apiImg=import.meta.env.VITE_API_IMAGE_URL
 
 
 export default function Movie() {
-   
+   const [currentPage,setCurrentPage]=useState(1)
     const dispatch=useDispatch()
+    const itemsPer=8
 
     const {data,status}=useSelector(state=>state.movie)
 
@@ -16,6 +17,10 @@ export default function Movie() {
         dispatch(getMovieApi())
     },[])
 
+    const totalPage=Math.ceil(data.length/itemsPer)
+    const currentData=data.slice((currentPage-1)*itemsPer,currentPage*itemsPer)
+    const per=[]
+   
 
    
     if(status==='fail'){
@@ -25,11 +30,15 @@ export default function Movie() {
         return <div>Loading</div>
     }
     if(status==='success'){
+        for(let i=1;i<=totalPage;i++){
+            per.push(i)
+            console.log(per)
+        }
         return (
             <div className="container mx-auto">
                 <div className="flex flex-wrap gap-2" >
-                    {data && data.map(d=>(
-                        <div key={d.id} className="w-30 h-50 bg-zinc-900 my-4  rounded-md group" >
+                    {data && currentData.map(d=>(
+                        <div key={d.id} className="w-[300px]  h-50 bg-zinc-900 my-4  rounded-md group" >
                             <img src={`${apiImg}/${d.poster_path}`} className="rounded-md group-hover:scale-x-110 transition-all duration-300"  />
                             <div className="flex flex-col">
                                 <h1 className="text-lg font-medium group-hover:text-xl  transition-all duration-200">{d.original_title}</h1>
@@ -40,6 +49,15 @@ export default function Movie() {
                         </div>
                     ))}
                 </div>
+                <ul className="flex items-center justify-center gap-4">
+                    {per.length>0 && per.map(item=>(
+                        <li key={item} className={` hover:bg-indigo-800 px-3 py-1 rounded-full font-medium ${item===currentPage ? 'bg-red-500' : 'bg-indigo-500'}`} onClick={()=>setCurrentPage(item)}>
+                           <button>
+                           {item}
+                           </button>
+                        </li>
+                    ))}
+                </ul>
             </div>
           )
     }
